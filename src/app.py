f4530b2d6b25f39cd0369143518a1b34525c62b3
jhocoder <jhonathanrugeb@gmail.com>
2025-01-31 12:18:17 +0100

    test api users

--- src/app.py
+++ src/app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/pythonMongodb"
 mongo = PyMongo(app)
+
+@app.route('/users', methods=["POST"])
+def create_user():
+    print(request.get_json)
+    return jsonify({"message": "ruta users"})
 
 
 
